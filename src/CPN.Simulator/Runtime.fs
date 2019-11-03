caea305ebe346cffd1a2d625d69fe816c196138c
Rodrigo Oliveri <rodrigooliveri10@gmail.com>
2019-11-03 02:11:39 -0300

    Added the step function that advance to the next state of the petri net; it was tested on the simpleNet and works. notSoSimpleNet added for testing

--- src/CPN.Simulator/Runtime.fs
+++ src/CPN.Simulator/Runtime.fs
@@ -37,15 +37,14 @@ type Arc = ArcStructure * Expression
 type CPN = Arc list
 /// Runtime module in charge of effectively do the simulation.
 module Runtime =
+    // Simple definitions for convinience
+    let (Ok unitCS) = Unit.create None
+    let unitColour = UnitCS unitCS
+    let unitToken = { qty = 1; value = "()"; colour = unitColour}
     
     /// Definition of the most simple petri net
     let simpleNet =
-        let (Ok unitCS) = Unit.create None
-
-        let unitColour = UnitCS unitCS
-
-        let unitToken = { qty = 1; value = "()"; colour = unitColour}
-       
+   
         let places = [|
             { name = "P1"; colour = unitColour; marking = [unitToken]}
             { name = "P2"; colour = unitColour; marking = []}
@@ -59,6 +58,34 @@ module Runtime =
         ]
 
         net
+    
+    let notSoSimpleNet = 
+        
+        let places = [|
+            { name = "P1"; colour = unitColour; marking = [{unitToken with qty = 3}]}
+            { name = "P2"; colour = unitColour; marking = [unitToken]}
+            { name = "P3"; colour = unitColour; marking = []}
+            { name = "P4"; colour = unitColour; marking = []}
+            { name = "P5"; colour = unitColour; marking = []}
+        |]
+
+        let transitions = [|
+            { name = "T1"; bindings = [] }
+            { name = "T2"; bindings = [] }
+        |]
+
+        let net : CPN = [
+            Input (places.[0], transitions.[0]), ""
+            Input (places.[1], transitions.[0]), ""
+            Output (transitions.[0], places.[1]), ""
+            Output (transitions.[0], places.[2]), ""
+            Input (places.[2], transitions.[1]), ""
+            Output (transitions.[1], places.[3]), ""
+            Output (transitions.[1], places.[4]), ""
+        ]
+
+        net
+
 
     /// parse a multi set color to string (this is needed because multiset are 
     /// not implemented yet)
@@ -75,18 +102,67 @@ module Runtime =
         net
         |> List.map (function Input (p, _), _-> p | Output (_, p), _ -> p )
         |> List.filter (fun p -> p.marking <> [])
+        |> List.distinct
 
     /// return the triggered transitions
-    let trigger (net: CPN) =
-        net
-        |> List.filter (function Input _, _-> true | _ -> false)
-        |> List.fold (fun (acc: Map<Transition,Place list>) (Input (place, trans), _) ->
+    let trigger (net: CPN) =       
+        let emptyMap: Map<Transition,Place list> = ([] |> Map.ofList)
+
+        let filteredNet = 
+            net |> List.filter (function Input _, _-> true | _ -> false)
+
+        emptyMap
+        |> List.foldBack (fun (Input (place, trans), _) acc ->
             match acc.TryFind trans with
             | None -> acc.Add (trans, [place])
             | Some placeList -> acc.Add (trans, place :: placeList)
-        ) ([] |> Map.ofList)
+        ) filteredNet
         |> Map.filter (fun _ placeList -> 
             placeList |> List.forall (fun p -> p.marking <> []))
+
+    /// remove the input tokens from the places involved in trigggering the 
+    /// transition.
+    let removeInputTokens (toTrigger: Map<Transition, Place list>) net : CPN =
+        net 
+        |> List.map (function
+            | Input (p, t), e when toTrigger.ContainsKey t ->
+                let newMarking = 
+                    p.marking
+                    |> List.head // Given it's only one type there is no need for more logic now
+                    |> function
+                        | {qty = 1} -> []
+                        | token -> [{token with qty = token.qty - 1}]
+
+                Input ({p with marking = newMarking} , t), e    
+            | otherwise -> otherwise)
+    
+    /// add the output tokens for the places reached by the triggered transition.
+    let addOutputTokens (toTrigger: Map<Transition, Place list>) net : CPN =
+        net 
+        |> List.map (function
+            | Output (t, p), e when toTrigger.ContainsKey t ->
+                let newMarking = 
+                    p.marking
+                    |> function
+                        | [] -> [unitToken]
+                        | token::rest -> {token with qty = token.qty + 1} :: rest
+                        // as in it's counterpart; it's simple beacause there is only 1 token
+
+                Output (t, {p with marking = newMarking}), e    
+            | otherwise -> otherwise)
+
+    /// make a step in the net
+    let step (net: CPN) =
+        let toTrigger = trigger net
+
+        match Map.isEmpty toTrigger with
+        | true -> false, net
+        | false ->
+            net 
+            |> removeInputTokens toTrigger 
+            |> addOutputTokens toTrigger
+            |> fun modifiedNet -> true, modifiedNet
+
         
 
 
--- tests/CPN.Simulator.Tests/RuntimeTests.fs
+++ tests/CPN.Simulator.Tests/RuntimeTests.fs
@@ -6,19 +6,54 @@ open CPN.Simulator
 
 module RuntimeTests =
 
+    open System.Collections.Generic
+    
+    let simplifyNetMarking =
+        List.map (fun (p: Place) -> p.name, Runtime.parseMultiSet p.marking)    
+
+    let simplifyTriggered =
+        Seq.map (fun (keyVal: KeyValuePair<Transition, Place list>) -> 
+            keyVal.Key.name, 
+            keyVal.Value |> List.map (fun p -> p.name))
+        >> Seq.toList
+    
     [<Tests>]
     let tests = 
         testList "RuntimeTests" [
             testCase "test the marking of the simple net to be P1 with 1 unit value" <| fun () ->
                 Runtime.simpleNet
                 |> Runtime.netMarking 
-                |> List.map (fun p -> p.name, Runtime.parseMultiSet p.marking)
+                |> simplifyNetMarking
                 =! ["P1", "1`()"]
+
+                Runtime.notSoSimpleNet
+                |> Runtime.netMarking 
+                |> simplifyNetMarking
+                =! [("P1", "3`()"); ("P2", "1`()")]
             
             testCase "test the triggered transitions" <| fun () ->
-                // TODO: further test this and make mor complicated nets
                 Runtime.simpleNet
                 |> Runtime.trigger
-                =! ([] |> Map.ofList)
+                |> simplifyTriggered
+                =! ["T1", ["P1"]]
 
+                Runtime.notSoSimpleNet
+                |> Runtime.trigger
+                |> simplifyTriggered
+                =! ["T1", ["P1"; "P2"]]
+
+            testCase "test the steps involved in the simple net" <| fun () ->
+                let modified, firstStepNet = Runtime.simpleNet |> Runtime.step
+
+                modified =! true 
+
+                firstStepNet 
+                |> Runtime.trigger 
+                |> simplifyTriggered
+                =! []
+
+                firstStepNet
+                |> Runtime.netMarking
+                |> simplifyNetMarking
+                =! ["P2", "1`()"]
         ]
