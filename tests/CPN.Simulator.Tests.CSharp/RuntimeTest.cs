9be7edff6081f57369c62cbf5ac1f4aaafb33ed5
Rodrigo Oliveri <rodrigooliveri10@gmail.com>
2019-11-18 00:12:06 -0300

    Better making of the net Test

--- tests/CPN.Simulator.Tests.CSharp/RuntimeTest.cs
+++ tests/CPN.Simulator.Tests.CSharp/RuntimeTest.cs
@@ -11,17 +11,26 @@ namespace CPN.Simulator.Tests.CSharp
         [Fact]
         public void NotSoSimpleNetTest()
         {
-            var result = Runtime.allSteps(SampleNets.notSoSimpleNet);
+            // The notSoSimpleNet has 5 steps until finishing
+            var allSteps = Runtime.allSteps(SampleNets.notSoSimpleNet);
 
-            Assert.Equal(5, result.Count());
+            Assert.Equal(5, allSteps.Count());
 
-            var lastMarking = CPNModule.netMarking(result.ElementAt(4));
+            // The last state of the net should be of 3 places marked, p2 with 
+            // 1`() and p4 & p5 with 3`()
+            var lastState = allSteps.ElementAt(allSteps.Count() - 1);
+            var lastStateMarking = CPNModule.netMarking(lastState);
 
-            Assert.Equal(3, lastMarking.Count());
+            Assert.Equal(3, lastStateMarking.Count());
 
-            Assert.Equal("1`()", lastMarking.ElementAt(0).Item2);
-            Assert.Equal("3`()", lastMarking.ElementAt(1).Item2);
-            Assert.Equal("3`()", lastMarking.ElementAt(2).Item2);
+            // The marking of p2, p4 and p5 is emulated here
+            var p2 = new Tuple<PlaceId, string>(PlaceId.NewP(2), "1`()");
+            var p4 = new Tuple<PlaceId, string>(PlaceId.NewP(4), "3`()");
+            var p5 = new Tuple<PlaceId, string>(PlaceId.NewP(5), "3`()");
+
+            Assert.Equal(p2, lastStateMarking.ElementAt(0));
+            Assert.Equal(p4, lastStateMarking.ElementAt(1));
+            Assert.Equal(p5, lastStateMarking.ElementAt(2));
         }
     }
 }
